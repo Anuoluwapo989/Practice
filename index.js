194de87543fd7f8c263c5f8aa2b9418063d6b28f
Anuoluwapo989 <114390341+Anuoluwapo989@users.noreply.github.com>
2024-11-19 20:31:25 -0500

    Implemented the flip animation for the cards.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Hala, you can work on the button for start and a function that takes it from page1.html to page2.html on click
+// Hala, you can work on the button for start and a function that takes it from page1.html to page2.html on click.
+//We need to have a function that spawns two of each card type onto the page at random slots
+//We also need a function that counts the users score and 
